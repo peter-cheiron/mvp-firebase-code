3e251a00f670f470c369ba92e4500b25f3625f45
peter-cheiron <peter.snowdon@cheiron.io>
2026-01-03 10:59:22 +0100

    Revert "solution chapter 2"
    This reverts commit 41190ae4cd00d6574349a6ab7b34b5a6e4e41507.
    
    trying to get prior state

--- mvp-with-angular-firebase/src/app/pages/activity-page/activity-page.ts
+++ mvp-with-angular-firebase/src/app/pages/activity-page/activity-page.ts
@@ -1,50 +1,23 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ActivityService } from '../../services/activity-service';
-import { DbActivityService } from '../../services/db-activity-service';
-import { Activity } from '../../models/activity';
-import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'activity-list',
   standalone: true,
-  imports: [RouterLink, NgClass],
+  imports: [RouterLink],
   templateUrl: "./activity-page.html" 
 })
 export class ActivityPage {
 
-activityService = inject(DbActivityService)
-  activites: Activity[] = [];
+  activityService = inject(ActivityService)
+  activites: any[] = [];
   
-  search = "";
-  categories: string[] = []
-  selected = ""
-  
-  constructor() {
-  }
+  constructor() {}
 
   //angular method called when a component is initialised
   ngOnInit(){
-    this.activityService.list().then(activities => {
-      this.activites = activities;
-      this.categories = [...new Set(
-        activities.map(a => a.category)
-      )];
-    })
+    this.activites = this.activityService.list()
   }
 
-  async filter(category:string){
-    if(this.selected === category){
-      this.selected = ""
-      this.activityService.list().then(activities => {
-        this.activites = activities;
-      })
-    }else{
-      this.selected = category;
-      const results = await this.activityService.filter("category", category);
-      if(results !== null){
-        this.activites = results;
-      }
-    }
-  }
 }
