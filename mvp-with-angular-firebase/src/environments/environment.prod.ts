cd6ab6b4631d0c721db35720e296537c35bfdd41
peter-cheiron <peter.snowdon@cheiron.io>
2026-01-03 15:32:48 +0100

    updating a lot

--- mvp-with-angular-firebase/src/environments/environment.prod.ts
+++ mvp-with-angular-firebase/src/environments/environment.prod.ts
@@ -2,13 +2,7 @@ export const environment = {
     production: true,
     environmentName: "production",
     firebaseConfig:{
-      apiKey: "AIzaSyB1EDySnaWcXQ6dk75iNvYYXek9SxhEdR8",
-      authDomain: "mvp-template-dbd61.firebaseapp.com",
-      projectId: "mvp-template-dbd61",
-      storageBucket: "mvp-template-dbd61.firebasestorage.app",
-      messagingSenderId: "398466912522",
-      appId: "1:398466912522:web:4651ee45e9f88d33c62af3",
-      measurementId: "G-4VNGL09QLV"
+
     }
   };
   
