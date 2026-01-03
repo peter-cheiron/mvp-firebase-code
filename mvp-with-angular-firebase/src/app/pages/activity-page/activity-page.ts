41190ae4cd00d6574349a6ab7b34b5a6e4e41507
peter-cheiron <peter.snowdon@cheiron.io>
2026-01-03 10:56:40 +0100

    solution chapter 2

--- mvp-with-angular-firebase/src/app/pages/activity-page/activity-page.ts
+++ mvp-with-angular-firebase/src/app/pages/activity-page/activity-page.ts
@@ -1,23 +1,50 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ActivityService } from '../../services/activity-service';
+import { DbActivityService } from '../../services/db-activity-service';
+import { Activity } from '../../models/activity';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'activity-list',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgClass],
   templateUrl: "./activity-page.html" 
 })
 export class ActivityPage {
 
-  activityService = inject(ActivityService)
-  activites: any[] = [];
+activityService = inject(DbActivityService)
+  activites: Activity[] = [];
   
-  constructor() {}
+  search = "";
+  categories: string[] = []
+  selected = ""
+  
+  constructor() {
+  }
 
   //angular method called when a component is initialised
   ngOnInit(){
-    this.activites = this.activityService.list()
+    this.activityService.list().then(activities => {
+      this.activites = activities;
+      this.categories = [...new Set(
+        activities.map(a => a.category)
+      )];
+    })
   }
 
+  async filter(category:string){
+    if(this.selected === category){
+      this.selected = ""
+      this.activityService.list().then(activities => {
+        this.activites = activities;
+      })
+    }else{
+      this.selected = category;
+      const results = await this.activityService.filter("category", category);
+      if(results !== null){
+        this.activites = results;
+      }
+    }
+  }
 }
