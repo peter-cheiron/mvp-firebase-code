6c546000def8035e626de9cd8e73bddcbeca95f6
peter-cheiron <peter.snowdon@cheiron.io>
2026-01-03 10:26:49 +0100

    creating chapter 2

---
+++ mvp-with-angular-firebase/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  environmentName: "development",
+  firebaseConfig:{
+  }
+};
--- mvp-with-angular-firebase/src/main.ts
+++ mvp-with-angular-firebase/src/main.ts
@@ -1,6 +1,44 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideFunctions, getFunctions } from '@angular/fire/functions';
+import { provideMessaging, getMessaging } from '@angular/fire/messaging';
+import { provideStorage, getStorage } from '@angular/fire/storage';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+import 'zone.js';
+
+import {
+  provideAnalytics,
+  getAnalytics,
+  ScreenTrackingService,
+  UserTrackingService,
+} from '@angular/fire/analytics';
+
+import { environment } from './environments/environment'; 
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {  importProvidersFrom } from '@angular/core';
+import { App } from './app/app';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes'
+
+bootstrapApplication(App, {
+  providers: [
+    importProvidersFrom(
+      BrowserModule,
+      FormsModule,
+      ReactiveFormsModule,
+    ),
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideAnalytics(() => getAnalytics()),
+    ScreenTrackingService,
+    UserTrackingService,
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions()),
+    provideMessaging(() => getMessaging()),
+    provideStorage(() => getStorage())
+  ],
+});
